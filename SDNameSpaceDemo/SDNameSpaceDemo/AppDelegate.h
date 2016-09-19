88f00c0decc294aed16073a60a20b1dfc85adc43
sand <sandhungry@126.com>
2016-09-19 15:02:54 +0800

    new pro

---
+++ SDNameSpaceDemo/SDNameSpaceDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDNameSpaceDemo
+//
+//  Created by 沙莎 on 16/9/19.
+//  Copyright © 2016年 沙莎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDNameSpaceDemo/SDNameSpaceDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SDNameSpaceDemo
+//
+//  Created by 沙莎 on 16/9/19.
+//  Copyright © 2016年 沙莎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
